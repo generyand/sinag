3fe8f8bbd3d1651adcf9ae7cba4d4f7439f21bf1
generyand <generyan.dep@gmail.com>
2025-12-09 13:21:38 +0800

    chore: remove leaflet map implementation and dependencies

--- apps/web/src/components/features/analytics/index.ts
+++ apps/web/src/components/features/analytics/index.ts
@@ -15,13 +15,13 @@ export * from "./hooks";
 export { ActiveFilterPills } from "./ActiveFilterPills";
 export { AnalyticsEmptyState } from "./AnalyticsEmptyState";
 export { AnalyticsSkeleton } from "./AnalyticsSkeleton";
-export { BarangayMap } from "./BarangayMap";
+
 export { GARAnalyticsTab } from "./GARAnalyticsTab";
 export { GlobalFilter } from "./GlobalFilter";
 export { PerformanceHotspotsWidget } from "./PerformanceHotspotsWidget";
 export { PerformanceOverviewWidget } from "./PerformanceOverviewWidget";
 export { SulopBarangayMap } from "./SulopBarangayMap";
+
 export { SulopBarangayMapIntegrated } from "./SulopBarangayMapIntegrated";
-export { SulopBarangayMapLeaflet } from "./SulopBarangayMapLeaflet";
 export { WeaknessDetailModal } from "./WeaknessDetailModal";
 export { WorkflowMetricsWidget } from "./WorkflowMetricsWidget";
