7c9c385e2331fa1e80e7fa600976e4e14ce76b59
generyand <generyan.dep@gmail.com>
2026-03-28 22:54:18 +0800

    fix(api): format alembic merge migration

--- apps/api/alembic/versions/8c6d4e887676_merge_sng_indicator_fixes_and_testing_.py
+++ apps/api/alembic/versions/8c6d4e887676_merge_sng_indicator_fixes_and_testing_.py
@@ -5,6 +5,7 @@ Revises: b7f2e1c4d8a9, b8c2d4e6f8a1
 Create Date: 2026-03-24 22:51:10.184779
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '8c6d4e887676'
-down_revision: Union[str, Sequence[str], None] = ('b7f2e1c4d8a9', 'b8c2d4e6f8a1')
+revision: str = "8c6d4e887676"
+down_revision: Union[str, Sequence[str], None] = ("b7f2e1c4d8a9", "b8c2d4e6f8a1")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
